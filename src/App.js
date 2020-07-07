31de94a4e0fbe897533e272ccb8e8041d316fa76
lucasvribeiro <lucasvribeiro14@gmail.com>
2020-07-07 10:47:48 -0300

    Ant design install

--- src/App.js
+++ src/App.js
@@ -1,36 +1,27 @@
 import React, { useEffect } from "react";
 import axios from "axios";
-import logo from "./logo.svg";
 import "./App.css";
+import { Button } from "antd";
+import 'antd/dist/antd.css';
 
-axios.defaults.baseURL = 'https://alerta-brumadinho-api.herokuapp.com/';
+axios.defaults.baseURL = "https://alerta-brumadinho-api.herokuapp.com/";
 // axios.defaults.baseURL = '';
 
 function App() {
   useEffect(() => {
-    axios
-      .get("post")
-      .then((res) => {
-        console.log(res);
-      });
+    axios.get("post").then((res) => {
+      console.log(res);
+    });
   }, []);
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Button type="primary">Primary Button</Button>
+      <Button>Default Button</Button>
+      <Button type="dashed">Dashed Button</Button>
+      <br />
+      <Button type="text">Text Button</Button>
+      <Button type="link">Link Button</Button>
     </div>
   );
 }
