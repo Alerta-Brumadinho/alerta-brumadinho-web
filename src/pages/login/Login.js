8eec528483e9850b43ffc8c12d75918c09bd141f
lucasvribeiro <lucasvribeiro14@gmail.com>
2020-07-08 12:41:08 -0300

    Finished Login Screen

--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -1,62 +1,105 @@
-import React from "react";
+import React, { Component } from "react";
 import { Row, Col, Input, Button } from "antd";
-import { UserOutlined, LockOutlined } from "@ant-design/icons";
+import {
+  UserOutlined,
+  LockOutlined,
+  EyeOutlined,
+  EyeInvisibleOutlined,
+} from "@ant-design/icons";
 
 import "./styles.css";
 
 const logo = require("../../assets/images/logo_512.png");
 
-const Login = () => {
-  return (
-    <div>
-      <Row justify="center" style={{ height: "38vh" }}>
-        <Col span={24} className="vertical-center">
-          <img src={logo} className="logo" alt="Alerta Brumadinho" />
-        </Col>
-      </Row>
+class Login extends Component {
+  login = () => {
+    console.log("login");
+  };
 
-      <Row justify="center" style={{ height: "8vh" }}>
-        <Col span={20} className="vertical-center">
-          <Input size="large" placeholder="E-mail" prefix={<UserOutlined />} />
-        </Col>
-      </Row>
+  register = () => {
+    console.log("register");
+  };
 
-      <Row justify="center" style={{ height: "8vh" }}>
-        <Col span={20} className="vertical-center">
-          <Input size="large" placeholder="Senha" prefix={<LockOutlined />} />
-        </Col>
-      </Row>
+  forgotPassword = () => {
+    console.log("forgotPassword");
+  };
 
-      <Row justify="center" style={{ height: "8vh" }}>
-        <Col span={10} className="vertical-center">
-          <a style={{ fontSize: "14px", textDecoration: "underline" }}>
-            Esqueceu sua Senha?
-          </a>
-        </Col>
-        <Col span={10} className="vertical-center">
-          <a style={{ fontSize: "14px", textDecoration: "underline" }}>
-            Acessar sem Cadastro
-          </a>
-        </Col>
-      </Row>
+  accessWithoutLogin = () => {
+    console.log("accessWithoutLogin");
+  };
 
-      <Row justify="center" style={{ height: "12vh" }}>
-        <Col span={20} className="vertical-bottom">
-          <Button type="primary" size="large" className="primary-button" block>
-            Entrar
-          </Button>
-        </Col>
-      </Row>
+  render() {
+    return (
+      <div>
+        <Row justify="center" style={{ height: "38vh" }}>
+          <Col span={24} className="vertical-center">
+            <img src={logo} className="logo" alt="Alerta Brumadinho" />
+          </Col>
+        </Row>
 
-      <Row justify="center" style={{ height: "10vh" }}>
-        <Col span={20} className="vertical-center">
-          <Button size="large" className="secondary-button" block>
-            Cadastrar
-          </Button>
-        </Col>
-      </Row>
-    </div>
-  );
-};
+        <Row justify="center" style={{ height: "8vh" }}>
+          <Col xs={20} sm={16} md={12} lg={8} className="vertical-center">
+            <Input
+              size="large"
+              placeholder="E-mail ou Telefone"
+              style={{ fontStyle: "italic" }}
+              prefix={<UserOutlined style={{ color: "#338221" }} />}
+            />
+          </Col>
+        </Row>
+
+        <Row justify="center" style={{ height: "8vh" }}>
+          <Col xs={20} sm={16} md={12} lg={8} className="vertical-center">
+            <Input.Password
+              size="large"
+              placeholder="Senha"
+              prefix={<LockOutlined style={{ color: "#338221" }} />}
+            />
+          </Col>
+        </Row>
+
+        <Row justify="center" style={{ height: "8vh" }}>
+          <Col xs={10} sm={8} md={6} lg={4} className="vertical-center">
+            <a href="#" style={{ textDecoration: "underline" }}>
+              Esqueceu sua Senha?
+            </a>
+          </Col>
+          <Col xs={10} sm={8} md={6} lg={4} className="vertical-center">
+            <a href="#" style={{ textDecoration: "underline" }}>
+              Acessar sem Cadastro
+            </a>
+          </Col>
+        </Row>
+
+        <Row justify="center" style={{ height: "12vh" }}>
+          <Col xs={20} sm={16} md={12} lg={8} className="vertical-bottom">
+            <Button
+              type="primary"
+              size="large"
+              className="primary-button"
+              onClick={this.login}
+              block
+            >
+              Entrar
+            </Button>
+          </Col>
+        </Row>
+
+        <Row justify="center" style={{ height: "10vh" }}>
+          <Col xs={20} sm={16} md={12} lg={8} className="vertical-center">
+            <Button
+              size="large"
+              className="secondary-button"
+              onClick={this.register}
+              block
+            >
+              Cadastrar
+            </Button>
+          </Col>
+        </Row>
+      </div>
+    );
+  }
+}
 
 export default Login;
