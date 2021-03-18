926a8412f3a06b448026760d6a671abad96ca46d
lucasvribeiro <lucasvribeiro14@gmail.com>
2021-03-18 10:17:23 -0300

    Home Screen Rework

--- src/App.js
+++ src/App.js
@@ -5,7 +5,8 @@ import { Switch, Route, Redirect, useLocation } from "react-router-dom";
 
 import "./App.less";
 
-import Login from "./pages/login/Login";
+import Home from "./pages/Home/Home";
+
 import RegisterProfile from "./pages/register/profile/RegisterProfile";
 import RegisterUser from "./pages/register/user/RegisterUser";
 import RegisterInstitution from "./pages/register/institution/RegisterInstitution";
@@ -18,18 +19,9 @@ axios.defaults.baseURL = "https://alerta-brumadinho-api.herokuapp.com";
 
 function App() {
   const location = useLocation();
-  // const transitions = useTransition(location, (location) => location.pathname, {
-  //   from: { opacity: 0, transform: "translate3d(100%,0,0)" },
-  //   enter: { opacity: 1, transform: "translate3d(0,0,0)" },
-  //   leave: { opacity: 0, transform: "translate3d(-50%,0,0)" },
-  //   trail: 0,
-  // });
-
   return (
-    // <animated.div key={key} style={props}>
-
     <Switch location={location}>
-      <Route path="/" exact component={Login} />
+      <Route path="/" exact component={Home} />
       <Route path="/register" exact component={RegisterProfile} />
       <Route path="/register/user" exact component={RegisterUser} />
       <Route
@@ -47,11 +39,10 @@ function App() {
         <Redirect to="/register" />
       </Route>
 
-      <Route path="/*" exact component={Login}>
+      <Route path="/*" exact component={Home}>
         <Redirect to="/" />
       </Route>
     </Switch>
-    // </animated.div>
   );
 }
 
