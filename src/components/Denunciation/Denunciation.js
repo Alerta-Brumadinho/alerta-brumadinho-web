a5862969abb8dbd4235fadebf562d39c8c66d0b0
lucasvribeiro <lucasvribeiro14@gmail.com>
2021-08-03 18:36:19 -0300

    denunciation date format

--- src/components/Denunciation/Denunciation.js
+++ src/components/Denunciation/Denunciation.js
@@ -9,6 +9,33 @@ const Denunciation = (props) => {
     console.log(props.denunciation);
   }, [props]);
 
+  
+const timeSince = (date) => {
+  var seconds = Math.floor((new Date() - date) / 1000);
+  var interval = seconds / 31536000;
+
+  if (interval > 1) {
+    return "há " + Math.floor(interval) + " anos";
+  }
+  interval = seconds / 2592000;
+  if (interval > 1) {
+    return "há " + Math.floor(interval) + " meses";
+  }
+  interval = seconds / 86400;
+  if (interval > 1) {
+    return "há " + Math.floor(interval) + " dias";
+  }
+  interval = seconds / 3600;
+  if (interval > 1) {
+    return "há " + Math.floor(interval) + " horas";
+  }
+  interval = seconds / 60;
+  if (interval > 1) {
+    return "há " + Math.floor(interval) + " minutos";
+  }
+  return "há " + Math.floor(seconds) + " segundos";
+}
+
   return (
     <Card
       size="small"
@@ -46,7 +73,7 @@ const Denunciation = (props) => {
           </div>
         </div>
 
-        <div>{props.denunciation.created}</div>
+        <div>{timeSince(new Date(props.denunciation.created))}</div>
       </div>
 
       <div className="denunciation-card-description">
